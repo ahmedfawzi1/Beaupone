0f79ec2a3e27ed5cbf186234c94279ad26079d5f
ahmedfawzi1 <ahmed.fawzii6102000@gmail.com>
2023-06-12 21:15:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+    $('.homeSlider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 3,
+        String: '50px',
+        boolean: true,
+        dots: true,
+        String: 'ondemand',
+        speed: 300,
+
+
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+});
+
+
+$(document).ready(function () {
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
+
